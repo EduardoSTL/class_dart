ba7ded637f5551f0d9a1d89403278200a2b68f50
EduardoSTL <eduardojosemelgar2004@gmail.com>
2023-09-06 09:11:08 -0600

    updt: getter and setter class

--- bin/getters_setters.dart
+++ bin/getters_setters.dart
@@ -7,18 +7,19 @@ void main(){
 }
 
 class Square{
-  double _side;
+  double _side; //side * side
 
   Square({required double side})
   : assert(side >= 0, 'side must be >= 0'),
   _side = side;
 
   double get area{
-    return _side * _side; //side * side
+    return _side * _side; 
   }
 
   set side(double value){
     print('setting new value $value');
     if (value <0) throw 'Value must be >= 0';
+    _side = _side;
   }
 }
